cfe8e4263706773df969f024dec48a863d2b6741
Henrique Dário <henrique.dariov@gmail.com>
2022-05-15 00:37:11 -0300

    Changed the Home and PokemonList pages, changed design and fixed some issues about Login Page

--- PokemonManager/ClientApp/src/components/Login.js
+++ PokemonManager/ClientApp/src/components/Login.js
@@ -1,65 +1,84 @@
 import React, { Component } from 'react';
 
 export class Login extends Component {
-  static displayName = Login.name;
+    static displayName = Login.name;
 
-  constructor(props) {
-      super(props);
-      this.state = { email: '', password: '' };
-      this.handleChangeEmail = this.handleChangeEmail.bind(this);
-      this.handleChangePassword = this.handleChangePassword.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' };
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
     handleChangeEmail(event) {
-        this.setState({ value: event.target.email });
+        this.setState({ email: event.target.value });
     }
     handleChangePassword(event) {
-        this.setState({ value: event.target.password });
+        this.setState({ password: event.target.value });
     }
 
     handleSubmit(event) {
-    event.preventDefault();
-    alert('Um nome foi enviado: ' + this.state.email);
-  }
+        event.preventDefault();
+        alert('Um nome foi enviado: ' + this.state.email);
+    }
 
 
-  render() {
-    return (
-        <form onSubmit={this.handleSubmit}>
-            <label style={styles.lblEmail}>
-                Email:
-            </label>
-            <input type="text" value={this.state.email} onChange={this.handleChangeEmail} />
-            <br/>
-            <label style={styles.lblPassword}>
-                Password:
-            </label>
-            <br />
-            <input type="password" value={this.state.password} onChange={this.handleChangePassword} />
-            <br />
-            <input type="submit" value="Enviar" />
-        </form>
-    );
-  }
+    render() {
+        return (
+            <div style={styles.form}>
+
+                <form onSubmit={this.handleSubmit} >
+                    <div class="form-group">
+                        <label style={styles.title}>
+                            Login
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            Email:
+                        </label>
+                        <input type="text" id="email" style={styles.txtArea} class="form-control" value={this.state.email} onChange={this.handleChangeEmail} />
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            Password:
+                        </label>
+
+                        <input type="password" id="password" style={styles.txtArea} class="form-control" value={this.state.password} onChange={this.handleChangePassword} />
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-success" style={styles.button} value="Login" />
+                    </div>
+
+                </form>
+            </div>
+        );
+    }
 }
 
 const styles = {
-    lblEmail: {
-        width: "100%",
-
+    form: {
+        display: "flex",
+        JustifyContent: "center",
+        alignItems: "center",
+        marginLeft: "35%",
     },
-    lblPassword: {
-        width: "30%",
+    txtArea: {
+        width: "120%",
     },
-    txtEmail: {
-        width: "100%",
-
+    button: {
+        width: "70%",
+        marginLeft: "25%",
+        marginTop: "3%",
     },
-    txtPassword: {
-        width: "30%",
-    },
-    btnSubmit: {
-        width: "30%",
+    title: {
+        width: "70%",
+        heigth: "50%",
+        fontSize: "40px",
+        fontWeight: "bold",
+        marginLeft: "35%",
+        marginBottom: "10%",
+        marginTop: "10%",
     }
 };
--- PokemonManager/ClientApp/src/components/PokemonList.js
+++ PokemonManager/ClientApp/src/components/PokemonList.js
@@ -14,7 +14,6 @@ export class PokemonList extends Component {
             <div>
                 <h1>Pokemon List</h1>
 
-                <p>This is a simple example of a React component.</p>
 
 
             </div>
